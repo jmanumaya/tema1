56fefb0741e9c8cb9b10c4d49abadcf755a9d173
jm.maya <jm.maya@106-20.info2>
2024-09-23 12:54:01 +0200

    Ejercicio 3 completado

--- src/parte1/Ejer3.java
+++ src/parte1/Ejer3.java
@@ -9,24 +9,26 @@ public class Ejer3 {
 		// Creo el scaner para que pueda recoger entrada de teclado.
 		Scanner sc = new Scanner (System.in);
 		
-		// Creo las variable para albergar los datos que el usuario nos introduzca
+		// Creo las variable para albergar los datos que el usuario nos introduzca.
 		int anoActual;
 		int anoNacimiento;
 		
 		// Le indicamos al usuario por pantalla
 		System.out.println("Buenas! Voy a adivinar cuantos años tienes...\nIndicame el año actual en el que estamos:");
 		
-		// Recogemos los datos que introduce el usuario por teclado
+		// Recogemos los datos que introduce el usuario por teclado en dicha variable.
 		anoActual = sc.nextInt();
 		
 		// Imprimimos por pantalla la siguiente indicación al usuario
 		System.out.println("Ahora, indicame el año de tu nacimiento:");
 		
-		// 
+		// Recogemos los datos que el usuario tiene que introducir en dicha variable.
 		anoNacimiento =  sc.nextInt();
 		
+		// Imprimimos por pantalla el resultado.
 		System.out.println("Tienes: " + (anoActual - anoNacimiento) + " años");
 		
+		// Cerramos el uso del escaner.
 		sc.close();
 
 	}
