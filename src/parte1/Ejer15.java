f63d008d5ccfbc8202930af32362b116a68e5d76
jmaya <jmaya@DESKTOP-QAQL6OH>
2024-09-25 23:18:07 +0200

    Merge branch 'master' of https://github.com/jmanumaya/tema1.git

--- src/parte1/Ejer15.java
+++ src/parte1/Ejer15.java
@@ -9,7 +9,7 @@ public class Ejer15 {
 		// Inicio el escaner para poder usarlo.
 		Scanner sc = new Scanner(System.in);
 
-		final double IVA = 1.21; // En el ejercicio dice de darle valor 21 pero supongo que te refieres a que sea 1.21 que es lo que se incrementa de IVA.
+		final double IVA = 21; // Declaro la constante IVA para que valga 21.
 									
 		double precio; // Declaramos la variable precios para albergar el dato del usuario.
 
@@ -17,9 +17,12 @@ public class Ejer15 {
 		System.out.println("Buenas, voy a calcular el precio total de un producto aplicandole el IVA.");
 		System.out.print("Dime que precio tiene tu producto sin IVA: ");
 		precio = sc.nextDouble(); // Entrada del dato del usuario por consola
+		
+		// Calcular el precio con IVA
+        double precioFinal = precio * (1 + IVA / 100);
 
 		// Le devuelvo al usuario el resultado de lo que le dije que haría el programa.
-		System.out.println("El precio total (IVA incluido) sería: " + (precio * IVA) + " Euros");
+		System.out.println("El precio total (IVA incluido) sería: " + precioFinal + " Euros");
 		
 		// Cerramos uso del escaner
 		sc.close();
